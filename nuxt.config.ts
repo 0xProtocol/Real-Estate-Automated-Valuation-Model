722ab358b751773500426588f047f506f55eb2b4
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-11-16 17:54:04 +0100

    build: install tailwind.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
 
-})
+export default defineNuxtConfig({
+    build: {
+        // @ts-ignore
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
+    },
+
+    css: [
+        '@/assets/css/main.css',
+    ],
+});
